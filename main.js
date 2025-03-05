a5ea5f27ceb6bab7829c59f577617f9067bf1b8c
May 12 <you@example.com>
2025-03-05 10:12:42 +0700

    bt chuong4

---
+++ main.js
@@ -0,0 +1,5 @@
+document.write("<h1> tạo button bằng script </h1><br>");
+document.write("<br>");
+document.getElementById('btn').addEventListener('click',function(){
+    document.getElementById('txtthongbao').value="hello";
+})
